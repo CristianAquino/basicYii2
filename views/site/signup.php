ca0b382c9419df87c2cb78828e9c650999bb46ce
cristianaquino <ycristian10@gmail.com>
2023-10-05 00:49:17 -0500

    feat: create view for user

---
+++ views/site/signup.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+
+<div class="site-signup">
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'email') ?>
+            <?= $form->field($model, 'password')->passwordInput() ?>
+            <div class="form-group">
+                <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+            </div>
+            <?php $form->end() ?>
+        </div>
+    </div>
+</div>
