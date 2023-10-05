bc8aa4afc04cb77d444ab3bba8e65384424172d2
cristianaquino <ycristian10@gmail.com>
2023-10-05 00:49:02 -0500

    feat: create model for user

---
+++ models/SignupForm.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use app\models\User;
+
+class SignupForm extends Model
+{
+    public $username;
+    public $email;
+    public $password;
+
+    public function rules()
+    {
+        return [
+            ['username', 'trim'],
+            ['username', 'required'],
+            ['username', 'unique', 'targetClass' => '\app\models\User', 'message' => 'this username has already been token.'],
+            ['username', 'string', 'min' => 2, 'max' => 255],
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => 255],
+            ['password', 'required'],
+            ['password', 'string', 'min' => 6]
+        ];
+    }
+
+    public function signup()
+    {
+        if (!$this->validate()) {
+            return null;
+        }
+        $user = new User();
+        $user->username = $this->username;
+        $user->email = $this->email;
+        $user->setPassword($this->password);
+        $user->generateAuthKey();
+        $user->generateEmailVerificationToken();
+        return $user->save();
+    }
+}
