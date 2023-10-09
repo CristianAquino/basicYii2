a8b540367900ee80ea15b95f12aba97df1b98710
cristianaquino <ycristian10@gmail.com>
2023-10-09 11:30:05 -0500

    feat: add crud to navbar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -46,6 +46,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
                 ['label' => 'Contact', 'url' => ['/site/contact']],
                 ['label' => 'FirstMVC', 'url' => ['/sitio/inicio']],
                 ['label' => 'Suscriptores', 'url' => ['/site/signup']],
+                ['label' => 'Libro', 'url' => ['/libro/index']],
                 Yii::$app->user->isGuest
                     ? ['label' => 'Login', 'url' => ['/site/login']]
                     : '<li class="nav-item">'
